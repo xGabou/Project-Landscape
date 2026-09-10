e2520f411de3122625779bb42a5e89f4aa0a0eea
Gaboouu <99047760+xGabou@users.noreply.github.com>
2026-09-10 12:22:51 -0400

    fix: replace unmapped Screen invoker with access transformer

--- src/main/java/raccoonman/reterraforged/client/gui/ColumnAlignment.java
+++ src/main/java/raccoonman/reterraforged/client/gui/ColumnAlignment.java
@@ -1,9 +1,9 @@
 package raccoonman.reterraforged.client.gui;
 
 import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
-import raccoonman.reterraforged.mixin.ScreenInvoker;
 
 public class ColumnAlignment {
 	private Screen parent;
@@ -22,17 +22,17 @@ public class ColumnAlignment {
 		this.left = horizontalMargin;
 	}
 	
-	public <T extends GuiEventListener & Renderable> T addColumn(float columnSize, ColumnFactory<T> factory) {
+	public <T extends GuiEventListener & Renderable & NarratableEntry> T addColumn(float columnSize, ColumnFactory<T> factory) {
 		int pageWidth = this.parent.width - (this.horizontalMargin * 2);
 		int height = this.parent.height;
 		int columnWidth = Math.max(0, Math.round(columnSize * pageWidth) - (2 * this.horizontalPadding));
 		T column = factory.apply(this.left, this.verticalMargin, columnWidth, height, this.horizontalPadding, this.verticalPadding);
 		this.left += columnWidth > 0 ? columnWidth + (2 * this.horizontalPadding) : 0;
-		((ScreenInvoker) this.parent).invokeAddRenderableWidget(column);
+		this.parent.addRenderableWidget(column);
 		return column;
 	}
 	
-	public interface ColumnFactory<T extends GuiEventListener> {
+	public interface ColumnFactory<T extends GuiEventListener & Renderable & NarratableEntry> {
 		T apply(int left, int top, int columnWidth, int height, int horizontalPadding, int verticalPadding);
 	}
 }
